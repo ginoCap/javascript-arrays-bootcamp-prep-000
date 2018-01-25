991b717f036526cfd0368f1626ac3b838cb43286
Yevgeniy Zhitomirskiy <yevgeniy.zhitomirskiy@wework.com>
2018-01-25 21:15:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,3 +10,9 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element);
   return array;
 }
+
+function addElementToEndOfArray(array, element){
+  var newArray = [...array];
+  newArray.push(element);
+  return newArray;
+}
