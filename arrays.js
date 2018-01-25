529f5298d9fd323b2c6810522fa673284e5b0092
Yevgeniy Zhitomirskiy <yevgeniy.zhitomirskiy@wework.com>
2018-01-25 20:43:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,10 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element){
-  
+  var newArray = array;
+  return newArray.push(element);
 }
 
-function 
+function destructivelyAddElementToBeginningOfArray(array, element){
+  return array.push(element);
+}
