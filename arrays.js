ffc435db1efd7b0e759d3ba93172547163d69a93
Yevgeniy Zhitomirskiy <yevgeniy.zhitomirskiy@wework.com>
2018-01-25 20:53:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
-var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+var chocolateBarfs = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element){
   var newArray = array;
